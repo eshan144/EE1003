b75ec656e503477440d1f30a0325351a3c3a3f80
eshan144 <eshan.mocake@gmail.com>
2025-02-04 18:19:22 +0530

    assign

--- 11.16.4.3.3/codes/data.c
+++ 11.16.4.3.3/codes/data.c
@@ -1,46 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-// Function to compute PMF
-double pmf(int n) {
-    if (n == 1) return 2.0 / 6.0;
-    else if (n == 2) return 3.0 / 6.0;
-    else if (n == 3) return 1.0 / 6.0;
-    else return 0.0;
-}
-
-// Function to compute CDF
-double cdf(int n) {
-    double sum = 0.0;
-    for (int i = 1; i <= n; i++) {
-        sum += pmf(i);
-    }
-    return sum;
-}
+#define N 100000 // Number of simulations
+#define RAND_MAX_FLOAT ((float)RAND_MAX)
 
 int main() {
-    // Open a file to write PMF and CDF values
-    FILE *file = fopen("data.txt", "w");
-    if (!file) {
-        perror("Error opening file");
+    int outcomes[] = {0, 1, 2, 3};  // Possible values of the random variable
+    float probs[] = {0.15, 0.35, 0.35, 0.15}; // Theoretical probabilities
+    int num_outcomes = sizeof(outcomes) / sizeof(outcomes[0]);
+
+    int count[4] = {0}; // Frequency of each outcome
+    float pmf[4], cdf[4];
+
+    srand(time(NULL));
+
+    // Simulation
+    for (int i = 0; i < N; i++) {
+        float r = (float)rand() / RAND_MAX_FLOAT;
+        float cumulative_prob = 0.0;
+
+        for (int j = 0; j < num_outcomes; j++) {
+            cumulative_prob += probs[j];
+            if (r < cumulative_prob) {
+                count[j]++;
+                break;
+            }
+        }
+    }
+
+    float cumulative = 0.0;
+    for (int j = 0; j < num_outcomes; j++) {
+        pmf[j] = (float)count[j] / N;
+        cumulative += pmf[j];
+        cdf[j] = cumulative;
+    }
+
+    FILE *file = fopen("results.txt", "w");
+    if (file == NULL) {
+        printf("Error opening file!\n");
         return 1;
     }
-
-    // Write PMF values to the file
-    fprintf(file, "PMF:\n");
-    for (int i = 1; i <= 3; i++) {
-        fprintf(file, "P(X = %d) = %.4f\n", i, pmf(i));
+    
+    fprintf(file, "x pmf_sim cdf_sim pmf_theory cdf_theory\n");
+    float cdf_theory = 0.0;
+    for (int j = 0; j < num_outcomes; j++) {
+        cdf_theory += probs[j];
+        fprintf(file, "%d %.5f %.5f %.5f %.5f\n", outcomes[j], pmf[j], cdf[j], probs[j], cdf_theory);
     }
 
-    // Write CDF values to the file
-    fprintf(file, "\nCDF:\n");
-    for (int i = 0; i <= 3; i++) {
-        fprintf(file, "P(X <= %d) = %.4f\n", i, cdf(i));
-    }
-
-    // Close the file
     fclose(file);
+    printf("Simulation data written to results.txt\n");
 
-    printf("Data written to data.txt successfully.\n");
     return 0;
 }
 
